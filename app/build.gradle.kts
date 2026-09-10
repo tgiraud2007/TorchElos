394c7db364140667ae0b32026188b44471828428
Thomas <thomas@noreply.github.com>
2026-09-10 21:40:02 +0200

    build: keep v1.2.0-beta version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 4
-        versionName = "1.2.1-beta"
+        versionName = "1.2.0-beta"
     }
 
     signingConfigs {
